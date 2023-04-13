4a3fbc41b6da391aede016aaa5ac98327e622b9e
rexhent <dhannah10@outlook.com>
2023-04-13 14:18:52 +1000

    first commit

---
+++ components/Content.jsx
@@ -0,0 +1,25 @@
+import styles from "../styles/Home.module.css";
+
+export function ContentCenter() {
+    return (
+        <div className={styles.contentCenter}>
+            <p>Hello World!</p>
+        </div>
+    )
+}
+
+export function ContentRight() {
+    return (
+        <div className={styles.contentRight}>
+            <p>Hello World!</p>
+        </div>
+    )
+}
+
+export function ContentLeft() {
+    return (
+        <div className={styles.contentLeft}>
+            <p>Hello World!</p>
+        </div>
+    )
+}
